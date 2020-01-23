085e1a8d1ed0b7efa51d2d73ab03423a64e38bbe
dongdang-cho <60133320+dongdang-cho@users.noreply.github.com>
2020-01-24 05:42:09 +0900

    메타정보를 로드하는 클래스

---
+++ Instar/src/main/java/util/MetaDataLoader.java
@@ -0,0 +1,66 @@
+package util;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class MetaDataLoader {
+	private static Properties pathProFile = new Properties();
+	private static Properties seleniumProfile = new Properties();
+	private static Properties urlProfile = new Properties();
+	private static Properties tagProfile = new Properties();
+	private static Properties accountProfile = new Properties();
+	static {
+		try {
+			pathProFile.load(new FileInputStream("path.properties"));
+			seleniumProfile.load(new FileInputStream("seleniumInfo.properties"));
+			urlProfile.load(new FileInputStream("url.properties")); 
+			tagProfile.load(new FileInputStream("tag.properties"));
+			accountProfile.load(new FileInputStream("account.properties"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+	}
+	/**
+	 * @return the pathProFile
+	 */
+	public static Properties getPathProFile() {
+		return pathProFile;
+	}
+	/**
+	 * @return the seleniumProfile
+	 */
+	public static Properties getSeleniumProfile() {
+		return seleniumProfile;
+	}
+	/**
+	 * @return the seedURLProfile
+	 */
+	public static Properties getSeedURLProfile() {
+		return urlProfile;
+	}
+	/**
+	 * @return the tagProfile
+	 */
+	public static Properties getTagProfile() {
+		return tagProfile;
+	}
+	/**
+	 * @return the idProfile
+	 */
+	public static Properties getIdProfile() {
+		return accountProfile;
+	}
+	
+	
+	
+	
+	
+}
